fd34e3502f4835835f0d8f70a8c294e50b27c3d4
sam <yungchinpang999@gmail.com>
2023-12-28 20:42:15 +0800

    nope i didnt fix it

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 
 export const revalidate = 60 * 60;
 export const dynamicParams = false;
+export const runtime = 'edge';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
