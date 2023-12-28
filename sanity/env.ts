01dbb735fc8b65df7e5e22df2674df9164f52328
sam <yungchinpang999@gmail.com>
2023-12-28 18:32:55 +0800

    inlined id and dataset

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,15 +1,9 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-12-24'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+export const dataset = "production"
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+export const projectId = "ywfq98mg";
 
 export const useCdn = false
 
